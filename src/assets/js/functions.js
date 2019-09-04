b731a14f039e6fbb6830ee638d069ed3e1c865f2
jhonifigueredo <jhoni_choc@hotmail.com>
2019-09-03 21:45:29 -0400

    Aula 01

---
+++ src/assets/js/functions.js
@@ -0,0 +1,14 @@
+let nome = document.querySelector("#nome");
+let peso = document.querySelector("#peso");
+let altura = document.querySelector("#altura");
+
+function calcularIMC(peso, altura){
+  return peso / (altura * altura);
+}
+
+document.querySelector("#btn-calcular").addEventListener("click",(event) => {
+  event.preventDefault();
+  let imc = calcularIMC(peso.value, altura.value);
+  console.log(imc.toFixed(2));
+});
+
